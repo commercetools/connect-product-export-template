7c6c46c64252b17600b4957f797c284663c67b81
King-Hin Leung <kinghin.leung@commercetools.com>
2023-08-01 15:01:15 +0200

    Initialize connect-search-creator

---
+++ package/connect-search-creator/src/cli.ts
@@ -0,0 +1,88 @@
+/* eslint-disable no-console */
+import { cac } from 'cac';
+import { Listr, type ListrTask } from 'listr2';
+import * as tasks from './tasks';
+import { throwIfNodeVersionIsNotSupported } from './validations';
+import { shouldUseYarn } from './utils';
+import processOptions from './process-options';
+import type { TCliCommandOptions } from './types';
+import pkgJson from '../package.json';
+
+throwIfNodeVersionIsNotSupported(process.versions.node, pkgJson.engines.node);
+
+const cli = cac('create-connect-app');
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', (err) => {
+    throw err;
+});
+
+const run = () => {
+    // Default command
+    cli
+        .command('[project-directory]')
+        .usage(
+            '[project-directory]\n\n  Bootstraps a new Connect Application project using one of the predefined templates.'
+        )
+        .option(
+            '--template <name>',
+            '(optional) The name of the template to install.',
+            { default: 'typescript' }
+        )
+        .option(
+            '--yes',
+            '(optional) If set, the prompt options with default values will be skipped.',
+            { default: false }
+        )
+        .action(async (projectDirectory, options: TCliCommandOptions) => {
+            if (!projectDirectory) {
+                cli.outputHelp();
+                return;
+            }
+
+            console.log('');
+            console.log(`Documentation available at https://docs.commercetools.com/`);
+            console.log('');
+
+            const taskOptions = await processOptions(projectDirectory, options);
+
+            // Only task is to download the templates
+            const taskList = new Listr(
+                [tasks.downloadTemplate(taskOptions)].filter(Boolean) as ListrTask[]
+            );
+
+            await taskList.run();
+            const useYarn = shouldUseYarn();
+
+            console.log('');
+            console.log(
+                `🎉 🎉 🎉 The Connect Application has been created in the "${taskOptions.projectDirectoryName}" folder.`
+            );
+            console.log('');
+            console.log(`To get started:`);
+            console.log(`$ cd ${taskOptions.projectDirectoryName}`);
+            console.log('');
+            console.log(`In this directory you'll find all three templates`);
+            console.log(`cd ['event', 'job', 'service']`);
+            console.log('');
+            console.log(
+                `$ ${useYarn ? 'yarn' : 'npm'} install && ${
+                    useYarn ? 'yarn' : 'npm'
+                } start:dev`
+            );
+
+            console.log('');
+            console.log(
+                `Visit https://docs.commercetools.com/connect for more info about developing Connect Applications. Enjoy 🚀`
+            );
+        });
+
+    cli.help();
+    cli.version(pkgJson.version);
+
+    cli.parse();
+};
+
+export default run;
---
+++ package/connect-search-creator/validations.ts
@@ -0,0 +1,54 @@
+import fs from 'fs';
+import semver from 'semver';
+import type { TCliCommandOptions } from './types';
+
+const availableTemplates = {
+    javascript: 'javascript',
+    typescript: 'typescript',
+} as const;
+
+const throwIfTemplateIsNotSupported = (
+    templateName: TCliCommandOptions['template']
+) => {
+    switch (templateName) {
+        case availableTemplates.javascript:
+        case availableTemplates.typescript:
+            break;
+        default: {
+            const templateNamesList = Object.keys(availableTemplates).toString();
+            throw new Error(
+                `The provided template name "${templateName}" does not exist. Available templates are "${templateNamesList}". Make sure you are also using the latest version of "@commercetools-frontend/create-connect-app".`
+            );
+        }
+    }
+};
+
+const throwIfProjectDirectoryExists = (dirName: string, dirPath: string) => {
+    if (fs.existsSync(dirPath)) {
+        throw new Error(
+            `A directory named "${dirName}" already exists at this location "${dirPath}". Please choose a different project name or remove the directory, then try running the command again.`
+        );
+    }
+};
+
+const throwIfNodeVersionIsNotSupported = (
+    currentNodeVersion: string,
+    expectedVersionRange: string
+) => {
+    const hasValidNodeVersion = semver.satisfies(
+        currentNodeVersion,
+        expectedVersionRange
+    );
+
+    if (!hasValidNodeVersion) {
+        throw new Error(
+            `You are running Node ${currentNodeVersion} but create-connect-app requires Node ${expectedVersionRange}. Please update your version of Node.`
+        );
+    }
+};
+
+export {
+    throwIfTemplateIsNotSupported,
+    throwIfProjectDirectoryExists,
+    throwIfNodeVersionIsNotSupported,
+};
