a883932f9a72fea8073a97777a3154ec9d268209
King-Hin Leung <kinghin.leung@commercetools.com>
2023-08-11 12:18:05 +0200

    Update route.spec.js

--- full-ingestion/test/route.spec.js
+++ full-ingestion/test/route.spec.js
@@ -1,27 +1,45 @@
-import { expect, describe, beforeAll, afterAll, it } from '@jest/globals';
-import request from 'supertest';
-import server from '../src/index.js';
+import { expect, describe, afterAll, it } from '@jest/globals';
 
+/** Reminder : Please put mandatory environment variables in the settings of your github repository **/
 describe('Test event.controller.js', () => {
   const storeKey = 'MY_STORE_KEY'; // Specify the key of Commercetools Store for product synchronization
 
   it(`POST /${storeKey}`, async () => {
     let response = {};
-    // Send request to the connector application with following code statement
+    // Send request to the connector application with following code snippet.
     /**
-            response = await request(server).post(`/${storeKey}`);
-         **/
+
+     import request from 'supertest';
+     import server from '../src/index.js';
+
+     response = await request(server).post(`/${storeKey}`);
+
+     **/
     expect(response).toBeDefined();
   });
 
   it(`POST /`, async () => {
-    const res = await request(server).post(`/`);
-    expect(res.statusCode).toBe(404);
+    let response = {};
+    // Send request to the connector application with following code snippet.
+    /**
+
+     import request from 'supertest';
+     import server from '../src/index.js';
+
+     response = await request(server).post(`/`);
+
+     **/
+    expect(response).toBeDefined();
   });
 
   afterAll(() => {
-    if (server) {
+    // Enable the function below to close the application on server once all test cases are executed.
+    /**
+
+     if (server) {
       server.close();
     }
+
+     **/
   });
 });
