ca82d15b5d81d0404fd7ba35885fd7207fe6d7d8
King-Hin Leung <kinghin.leung@commercetools.com>
2023-08-14 12:37:47 +0200

    Update route.spec.js

--- full-ingestion/test/route.spec.js
+++ full-ingestion/test/route.spec.js
@@ -1,4 +1,6 @@
 import { expect, describe, afterAll, it } from '@jest/globals';
+import request from 'supertest';
+import server from '../src/index.js';
 
 /** Reminder : Please put mandatory environment variables in the settings of your github repository **/
 describe('Test event.controller.js', () => {
@@ -7,39 +9,26 @@ describe('Test event.controller.js', () => {
   it(`POST /${storeKey}`, async () => {
     let response = {};
     // Send request to the connector application with following code snippet.
-    /**
 
-     import request from 'supertest';
-     import server from '../src/index.js';
+    response = await request(server).post(`/${storeKey}`);
 
-     response = await request(server).post(`/${storeKey}`);
-
-     **/
     expect(response).toBeDefined();
   });
 
   it(`POST /`, async () => {
     let response = {};
     // Send request to the connector application with following code snippet.
-    /**
 
-     import request from 'supertest';
-     import server from '../src/index.js';
+    response = await request(server).post(`/`);
 
-     response = await request(server).post(`/`);
-
-     **/
     expect(response).toBeDefined();
   });
 
   afterAll(() => {
     // Enable the function below to close the application on server once all test cases are executed.
-    /**
 
-     if (server) {
+    if (server) {
       server.close();
     }
-
-     **/
   });
 });
