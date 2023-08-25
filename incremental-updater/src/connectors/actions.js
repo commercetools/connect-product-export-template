f84df0d69dbf90cdc3d7e6dbcc889710cafc5655
King-Hin Leung <kinghin.leung@commercetools.com>
2023-08-25 11:43:11 +0200

    Add post-deployment scripts

---
+++ incremental-updater/src/connectors/actions.js
@@ -0,0 +1,59 @@
+
+const CUSTOMER_CREATE_SUBSCRIPTION_KEY =
+    'myconnector-customerCreateSubscription';
+
+export async function createChangedStoreSubscription(
+    apiRoot,
+    topicName,
+    projectId
+) {
+    const {
+        body: { results: subscriptions },
+    } = await apiRoot
+        .subscriptions()
+        .get({
+            queryArgs: {
+                where: `key = "${CUSTOMER_CREATE_SUBSCRIPTION_KEY}"`,
+            },
+        })
+        .execute();
+
+    if (subscriptions.length > 0) {
+        const subscription = subscriptions[0];
+
+        await apiRoot
+            .subscriptions()
+            .withKey({ key: CUSTOMER_CREATE_SUBSCRIPTION_KEY })
+            .delete({
+                queryArgs: {
+                    version: subscription.version,
+                },
+            })
+            .execute();
+    }
+
+    await apiRoot
+        .subscriptions()
+        .post({
+            body: {
+                key: CUSTOMER_CREATE_SUBSCRIPTION_KEY,
+                destination: {
+                    type: 'GoogleCloudPubSub',
+                    topic: topicName,
+                    projectId,
+                },
+                changes : [
+                    {
+                        resourceTypeId: 'product'
+                    },
+                    {
+                        resourceTypeId: 'product-selection'
+                    },
+                    {
+                        resourceTypeId : 'store'
+                    }
+                ],
+            },
+        })
+        .execute();
+}
