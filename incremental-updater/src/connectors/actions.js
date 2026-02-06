f432ca2efdcde9443cde5fc7c465dbd5f0b5a9c3
Dario Ocles <Dario Ocles>
2026-02-06 16:20:00 +0100

    fix: correct GCP suscription name

--- incremental-updater/src/connectors/actions.js
+++ incremental-updater/src/connectors/actions.js
@@ -37,7 +37,7 @@ function buildDestination(config) {
   );
 
   switch (config.connectSubscriptionDestination) {
-    case 'GCP':
+    case 'GoogleCloudPubSub':
       assertNonNullable(
         config.connectGcpTopicName,
         'CONNECT_GCP_TOPIC_NAME is required for GCP destination'
@@ -63,7 +63,7 @@ function buildDestination(config) {
       };
     default:
       throw new Error(
-        `Unsupported subscription destination: ${config.connectSubscriptionDestination}. Valid options are 'GCP' or 'SNS'.`
+        `Unsupported subscription destination: ${config.connectSubscriptionDestination}. Valid options are 'GoogleCloudPubSub' or 'SNS'.`
       );
   }
 }
